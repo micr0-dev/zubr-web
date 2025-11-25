23a2b6c72ba2dac75e65a285399758e912e1dc42
Micr0Arm64 <26364458+micr0-dev@users.noreply.github.com>
2025-11-26 04:13:29 +1030

    this should work

--- defaults/remote-server.js
+++ defaults/remote-server.js
@@ -8,36 +8,36 @@
  */
 
 module.exports = {
-	// Connection settings - always secure
-	port: 6697,
-	tls: true,
-	rejectUnauthorized: true,
-	password: "",
+    // Connection settings - always secure
+    port: 6697,
+    tls: true,
+    rejectUnauthorized: false,
+    password: "",
 
-	// User identity (will be set dynamically using home instance domain)
-	nick: "",  // Format: username_domain with periods as hyphens
-	username: "", // Format: username_domain with underscore separator
-	realname: "", // Format: username@domain with at symbol separator
+    // User identity (will be set dynamically using home instance domain)
+    nick: "",  // Format: username_domain with periods as hyphens
+    username: "", // Format: username_domain with underscore separator
+    realname: "", // Format: username@domain with at symbol separator
 
-	// SASL - disabled by default
-	sasl: "",
-	saslAccount: "",
-	saslPassword: "",
+    // SASL - disabled by default
+    sasl: "",
+    saslAccount: "",
+    saslPassword: "",
 
-	// Channels - none by default
-	join: "",
+    // Channels - none by default
+    join: "",
 
-	// Messages
-	leaveMessage: "",
+    // Messages
+    leaveMessage: "",
 
-	// Proxy - disabled
-	proxyEnabled: false,
-	proxyHost: "",
-	proxyPort: 1080,
-	proxyUsername: "",
-	proxyPassword: "",
+    // Proxy - disabled
+    proxyEnabled: false,
+    proxyHost: "",
+    proxyPort: 1080,
+    proxyUsername: "",
+    proxyPassword: "",
 
-	// Other
-	commands: [],
-	ignoreList: [],
+    // Other
+    commands: [],
+    ignoreList: [],
 };
